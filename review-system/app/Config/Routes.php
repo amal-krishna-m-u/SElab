85c0fa3bf4591f839f89deab5f73666b9d7d6adc
amal-krishna-m-u <amalkrishna@ug.cusat.ac.in>
2023-02-13 22:54:23 +0530

    created the routes

--- review-system/app/Config/Routes.php
+++ review-system/app/Config/Routes.php
@@ -39,32 +39,59 @@ $routes->get('/', 'Home::index');
 
 //admin controller view routes 
 
-$routes->get('/Admin/login', 'Admin\Admin::loginview');
-$routes->get('/Admin/dashboard', 'Admin\Admin::dashboardview');
-$routes->get('/Admin/register', 'Admin\Admin::registerview');
-
+$routes->get('/Admin/register', 'Admin\Admin::registerView');
+$routes->get('/Admin/login', 'Admin\Admin::loginView');
+$routes->get('/Admin/dashboard', 'Admin\Admin::dashboardView');
 
 //admin controller model routes 
 $routes->post('/Admin/register', 'Admin\Admin::register');
-
+$routes->post('/Admin/login','Admin\Admin::login');
+$routes->post('/Admin/logout','Admin\Admin::logout');
 
 //user controller view routes
-$routes->get('/User/login', 'User\User::loginview');
-$routes->get('/User/register', 'User\User::registerview');
+$routes->get('/User/login', 'User\User::loginView');
+$routes->get('/User/register', 'User\User::registerView');
+$routes->get('/User/dashboard','User\User::dashboardView');
+$routes->get('/User/editDetails','User\User::editDetailsView');
 
 
 
 
 //user controller model routes
 
+$routes->post('/User/login','User\User::login');
+$routes->post('/User/register','User\User::register');
+$routes->post('/User/dashboard','User\User::dashbaord');
+$routes->post('/User/editDetails','User\User::editDetails');
 
 
 //Application controller view routes 
-$routes->get('/Application/listApplications','Applications\Applications::listapplicationview');
 
-
-
+$routes->get('/Application/editCategory','Application\Application::editCategoryView');
+$routes->get('/Application/deleteCategory', 'Application\Application::deleteCategoryView');
+$routes->get('/Application/addCategory','Application\Application::addCategoryView');
+$routes->get('/Application/selectCategory','Application\Application::selectCategoryView');
+$routes->get('/Application/listApplication','Application\Application::listApplicationView');
+$routes->get('/Application/listOfApplicationToVerifyView','Application\Application::listOfApplicationToVerifyView');
+$routes->get('/Application/addApplication','Application\Application::addApplicationView');
+$routes->get('/Application/applicationLeaderbaord','Application\Application::applicationLeaderboardView');
+$routes->get('/Application/infoApplication','Application\Application::infoApplicationView');
+$routes->get('/Application/oldReviewRating','Application\Application::oldReviewRatingView');
+$routes->get('/Application/newReviewRating','Application\Application::newReviewRatingView');
+$routes->get('/Application/searchLeaderboard','Application\Application::serachLeaderboardView');
 // Application controller model routes 
+$routes->post('/Application/editCategory','Application\Application::editCategory');
+$routes->post('/Application/deleteCategory', 'Application\Application::deleteCategory');
+$routes->post('/Application/addCategory','Application\Application::addCategory');
+$routes->post('/Application/selectCategory','Application\Application::selectCategory');
+$routes->post('/Application/listApplication','Application\Application::listApplication');
+$routes->post('/Application/listOfApplicationToVerifyView','Application\Application::listOfApplicationToVerify');
+$routes->post('/Application/addApplication','Application\Application::addApplication');
+$routes->post('/Application/applicationLeaderbaord','Application\Application::applicationLeaderboard');
+$routes->post('/Application/infoApplication','Application\Application::infoApplication');
+$routes->post('/Application/oldReviewRating','Application\Application::oldReviewRating');
+$routes->post('/Application/newReviewRating','Application\Application::newReviewRating');
+$routes->post('/Application/searchLeaderboard','Application\Application::serachLeaderboard');
 
 
 
