a505d50abbc271cd1a2ca7d6e52cea50c6b935f6
amal-krishna-m-u <amalkrishna@ug.cusat.ac.in>
2023-02-18 10:21:55 +0530

    userdetails update completed

---
+++ review-system/app/Models/Application.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+use CodeIgniter\Log\Logger;
+//$log = service('logger');
+
+
+class User extends Model
+{
+    protected $table = 'category';
+    protected $primaryKey = 'catid'; // Replace 'id' with the primary key field of your table
+    protected $allowedFields = ['catname']; // Replace with the field names of your table that you want to allow for updates
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
