1fdcac26ffc766980d4b217c3b0c4c33260b543e
amal-krishna-m-u <amalkrishna@ug.cusat.ac.in>
2023-02-18 17:35:59 +0530

    leaderboard

--- review-system/app/Views/user/dashboard.php
+++ review-system/app/Views/user/dashboard.php
@@ -17,7 +17,24 @@
     <p><?php echo $password; ?></p>
 
     <a href="/User/editDetails">Edit Details</a>
-    <a href="/Application/selectCategory">Select category</a>
+    <a href="/Application/selectCategory"> Select category </a>
+    <br>
+    <form action= "<?= base_url('/Application/applicationLeaderbaord')"  method = post >
+    <select name="category" id="category">
+    <?php 
+                
+        $db = mysqli_connect('localhost', 'root', '', 'review_system');
+        $sql = "SELECT * FROM category";
+        $result = mysqli_query($db, $sql);
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        foreach($categories as $category): ?>
+        <option value="<?= $category['id'] ?>"><?= $category['catname'] ?></option>
+    </select>
+    <input type="submit" value="Submit">
+    <a href="/Application/submitReview"> Review Detials </a>
+    <a href="/Application/viewReviews"> Add Application </a>
+    <a href="/Application/viewReviews"> View Reviews </a>
+    
     <a href="/User/login">Logout</a>
 
 
