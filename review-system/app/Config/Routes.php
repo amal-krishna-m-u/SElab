e242e2e7c0b0a495656d0070d0884ac5f6b914c4
amal <amalkrishna@ug.cusat.ac.in>
2023-02-19 09:51:16 +0530

    creaeted rotues

--- review-system/app/Config/Routes.php
+++ review-system/app/Config/Routes.php
@@ -70,13 +70,18 @@ $routes->post('/User/editDetails','User\User::editDetails');
 
 $routes->get('/Application/selectCategory','Application\Application::selectCategoryView');
 $routes->get('/Application/applicationLeaderbaord','Application\Application::applicationLeaderboardView');
-
+$routes->get('/Application/review','Application\Application::reviewView');
+$rotues->get('/Application/rate','Application\Application::rateView');
+$routes->get('/Application/moreinfo','Application\Application::moreInfoView');
 
 // Application controller model routes 
 
 $routes->post('/Application/selectCategory','Application\Application::selectCategory');
 $routes->post('/Application/applicationLeaderbaord','Application\Application::applicationLeaderboard');
 $routes->post('/Aplication/leaderboardSearch','Application\Application::leaderboardSearch');
+$routes->get('/Application/review','Application\Application::reviewView');
+$rotues->get('/Application/rate','Application\Application::rateView');
+$routes->get('/Application/moreinfo','Application\Application::moreInfoView');
 
 
 
